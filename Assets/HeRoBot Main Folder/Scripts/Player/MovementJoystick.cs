e4b8185ea004c56cdc9f7b910b5ca227290cfcfa
diliupg <diliupg@gmail.com>
2020-12-18 20:50:53 +0530

    joystick sprites added

---
+++ Assets/HeRoBot Main Folder/Scripts/Player/MovementJoystick.cs
@@ -0,0 +1,79 @@
+﻿/*
+==========================
+Copyright (c) Diliupg 2020
+   www.soft.diliupg.com
+==========================
+*/
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MovementJoystick : MonoBehaviour
+{
+    #region Public Fields
+
+    public GameObject joystick;
+    public GameObject joystickBG;
+    public Vector2 joystickVector;
+    public Vector2 joystickTouchPos;
+    public Vector2 joystickOriginalPos;
+    public float joystickRadius;
+
+    #endregion
+	
+    void Start()
+    {
+        joystickOriginalPos = joystickBG.transform.position;
+        joystickRadius = joystickBG.GetComponent<RectTransform> ( ).sizeDelta.y / 4;
+    }
+
+    public void PointerDown()
+    {
+        joystick.transform.position = Input.mousePosition;
+        joystickBG.transform.position = Input.mousePosition;
+        joystickTouchPos = Input.mousePosition;
+    }
+
+    public void Drag(BaseEventData baseEventData)
+    {
+        PointerEventData pointerEventData = baseEventData as PointerEventData;
+        Vector2 dragPos = pointerEventData.position;
+        joystickVector = ( dragPos - joystickTouchPos ).normalized;
+
+        float joystickDist = Vector2.Distance ( dragPos, joystickTouchPos );
+
+        if(joystickDist < joystickRadius)
+        {
+            joystick.transform.position = joystickTouchPos + joystickVector * joystickDist;
+        }
+        else
+        {
+            joystick.transform.position = joystickTouchPos + joystickVector * joystickRadius;
+        }
+    }
+
+    public void PointerUP()
+    {
+        joystickVector = Vector2.zero;
+        joystick.transform.position = joystickOriginalPos;
+        joystickBG.transform.position = joystickOriginalPos;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/_Unity Store Assets/Editor/NewBehaviourScript1.cs
@@ -0,0 +1,38 @@
+﻿/*
+==========================
+Copyright (c) Diliupg 2020
+   www.soft.diliupg.com
+==========================
+*/
+
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Editor
+{
+//    [InitializeOnLoad]
+//public class AutoLayoutChanger
+//    {
+//        [System.Obsolete]
+//        static AutoLayoutChanger ( )
+//        {
+
+//        EditorApplication.playmodeStateChanged = HandleOnPlayModeChanged;
+//        }
+
+//       static void HandleOnPlayModeChanged ( )
+//       {
+//        // This method is run whenever the playmode state is changed.
+//              if ( EditorApplication.isPlaying )
+//             {
+//            EditorUtility.LoadWindowLayout ( "Landscape Layout" );
+//            // do stuff when the editor is played.
+//            }
+//            else if ( !EditorApplication.isPaused )
+//             {
+//            EditorUtility.LoadWindowLayout ( "Tall Layout" );
+//            // other than playmode
+//             }
+//        }
+//    }
+}
