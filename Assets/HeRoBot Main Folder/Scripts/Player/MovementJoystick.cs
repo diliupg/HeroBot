4378381b225f82d46b9333750b1937f73459c360
diliupg <diliupg@gmail.com>
2020-12-19 15:05:42 +0530

    mobile input script to all buttons. new text field to show bool status

--- Assets/HeRoBot Main Folder/Scripts/Player/MovementJoystick.cs
+++ Assets/HeRoBot Main Folder/Scripts/Player/MovementJoystick.cs
@@ -15,17 +15,30 @@ public class MovementJoystick : MonoBehaviour
 
     public GameObject joyS;
     public GameObject joySBorder;
-    [SerializeField] private Text text;
+    [SerializeField] private Text butStatus;
+    [SerializeField] private Text boolStatus;
 
     public Vector2 joySVec2;
     public Vector2 joySTouchPos;
     public Vector2 joySStartPos;
     public float joySRadius;
 
-    
+    [HideInInspector] public float directionLR;      //Float that stores horizontal input
+    [HideInInspector] public float directionUD;    // float that stores the vertical input
+
+    [HideInInspector] public bool jumpHeld;         //Bool that stores jump pressed
+    [HideInInspector] public bool jumpPressed;      //Bool that stores jump held
+    [HideInInspector] public bool crouchHeld;       //Bool that stores crouch pressed
+    [HideInInspector] public bool crouchPressed;    //Bool that stores crouch held
+    [HideInInspector] public bool shootPressed;    //Bool that stores shoot pressed
+    [HideInInspector] public bool dashPressed;    //Bool that stores dash pressed
+    [HideInInspector] public bool torchPressed;    //Bool that stores torch pressed
+    [HideInInspector] public bool pausedPressed;    //Bool that stores escape pressed
+
+
 
     #endregion
-	
+
     void Start()
     {
         joySStartPos = joySBorder.transform.position;
@@ -37,7 +50,7 @@ public class MovementJoystick : MonoBehaviour
         joyS.transform.position = Input.mousePosition;
         joySBorder.transform.position = Input.mousePosition;
         joySTouchPos = Input.mousePosition;
-        text.text ="finger Down";
+        butStatus.text ="finger Down "+ gameObject.name;
     }
 
     public void Drag(BaseEventData baseEventData)
@@ -45,7 +58,7 @@ public class MovementJoystick : MonoBehaviour
         PointerEventData pointerEventData = baseEventData as PointerEventData;
         Vector2 dragPos = pointerEventData.position;
         joySVec2 = ( dragPos - joySTouchPos ).normalized;
-        //Debug.Log ( "joystick vector "+joystickVector );
+
         float joystickDist = Vector2.Distance ( dragPos, joySTouchPos );
 
         if(joystickDist < joySRadius)
@@ -56,7 +69,7 @@ public class MovementJoystick : MonoBehaviour
         {
             joyS.transform.position = joySTouchPos + joySVec2 * joySRadius;
         }
-        text.text = ( "joystick trans. Pos "+joyS.transform.position );
+        butStatus.text = ( "joystick vector " + joySVec2 );
     }
 
     public void PointerUP()
@@ -64,6 +77,63 @@ public class MovementJoystick : MonoBehaviour
         joySVec2 = Vector2.zero;
         joyS.transform.position = joySStartPos;
         joySBorder.transform.position = joySStartPos;
-        text.text = ( "finger Up" );
+        butStatus.text = ( "finger Up "+ gameObject.name );
     }
+
+    private void SetPressed()
+    {
+        if(gameObject.name == "Fire")
+        {
+            shootPressed = true;
+        }
+        
+        else if( gameObject.name == "Jump" )
+        {
+            jumpPressed = true;
+        }
+
+        else if ( gameObject.name == "Crouch" )
+        {
+            crouchPressed = true;
+        }
+
+        else if ( gameObject.name == "Dash" )
+
+        {
+            dashPressed = true;
+        }
+
+        else if ( gameObject.name == "Torch" && !torchPressed )
+        {
+            torchPressed = true;
+        }
+        else if( gameObject.name == "Torch" && torchPressed )
+        {
+            torchPressed = false;
+        }
+
+        else if ( gameObject.name == "Pause" )
+        {
+            pausedPressed = true;
+        }
+
+        boolStatus.text = gameObject.name + " pressed";
+    }
+
+    void ClearPressed()
+    {
+        if ( gameObject.name == "Fire" )
+        {
+            shootPressed = false;
+        }
+
+        else if ( gameObject.name == "Crouch" )
+        {
+            crouchPressed = false;
+        }
+
+        boolStatus.text = gameObject.name + " released";
+    }
+
+
 }
