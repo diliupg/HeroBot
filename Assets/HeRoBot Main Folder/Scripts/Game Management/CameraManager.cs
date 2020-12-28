ba9ba39c57a61a8a05ee56c1c2481fd0d9138c55
diliupg <diliupg@gmail.com>
2020-12-28 13:36:23 +0530

    call CompMan.DTCalc from GWM only before saving data.

--- Assets/HeRoBot Main Folder/Scripts/Game Management/CameraManager.cs
+++ Assets/HeRoBot Main Folder/Scripts/Game Management/CameraManager.cs
@@ -22,20 +22,9 @@ public class CameraManager : MonoBehaviour
         GameManager.RegisterCameraManager ( this );
 
         NormalVCam.Priority = camNormal;
-        //ZoomUpVCam.Priority = camUp;
-        //ZoomDownVCam.Priority = camDown;
+ 
         centered = true;
 
-    }
-
-    private void OnEnable ( )
-    {
-        //PlayerController.UpOrDownPressed += CameraLook;
-    }
-
-    private void OnDisable ( )
-    {
-        //PlayerController.UpOrDownPressed -= CameraLook;
     }
 
     void CameraLook ( float value )
@@ -75,11 +64,4 @@ public class CameraManager : MonoBehaviour
             //SetCamPriority ( );
         }
     }
-
-    //void SetCamPriority()
-    //{
-    //    //ZoomUpVCam.Priority = camUp;
-    //    //ZoomDownVCam.Priority = camDown;
-    //}
-
 }
