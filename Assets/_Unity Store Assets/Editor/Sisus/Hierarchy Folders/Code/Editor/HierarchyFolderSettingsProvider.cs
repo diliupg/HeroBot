cfa9d52148321729378712b05f3a87b001f37a8b
diliupg <diliupg@gmail.com>
2021-01-08 23:12:38 +0530

    removed old wall

---
+++ Assets/_Unity Store Assets/Editor/Sisus/Hierarchy Folders/Code/Editor/HierarchyFolderSettingsProvider.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using JetBrains.Annotations;
+
+namespace Sisus.HierarchyFolders
+{
+	/// <summary>
+	/// Handles drawing preferences view for Hierarchy Folders in window that can be opened via Edit > Preferences.
+	/// </summary>
+	public static class HierarchyFolderSettingsProvider
+	{
+		private static Editor defaultEditor;
+
+		#if UNITY_2019_1_OR_NEWER
+		[SettingsProvider, UsedImplicitly]
+		private static SettingsProvider CreateHierarchyFolderDrawer()
+		{
+			var provider = new SettingsProvider("Preferences/Hierarchy Folders", SettingsScope.User)
+			{
+				label = "Hierarchy Folders",
+				guiHandler = DrawPreferencesGUI,
+
+				// Populate the search keywords to enable smart search filtering and label highlighting
+				keywords = new System.Collections.Generic.HashSet<string>(new[]
+				{
+					"defaultName",
+					"namePrefix",
+					"nameSuffix",
+					"removeWhenEnterPlayMode",
+					"removeWhenMakingBuild",
+					"infoBoxText"
+				})
+			};
+
+			return provider;
+		}
+		
+		private static void DrawPreferencesGUI(string searchContext)
+		{
+			DrawPreferencesGUI();
+		}
+		#endif
+
+		#if !UNITY_2019_1_OR_NEWER
+		[PreferenceItem("Hierarchy Folders"), UsedImplicitly]
+		#endif
+		private static void DrawPreferencesGUI()
+		{
+			var labelWidthWas = EditorGUIUtility.labelWidth;
+			var indentLevelWas = EditorGUI.indentLevel;
+			EditorGUIUtility.labelWidth = 255f;
+			EditorGUI.indentLevel = 1;
+
+			var preferences = HierarchyFolderPreferences.Get();
+
+			Editor.CreateCachedEditor(preferences, null, ref defaultEditor);
+
+			string previousNamePrefix = preferences.namePrefix;
+			string previousNameSuffix = preferences.nameSuffix;
+
+			EditorGUI.BeginChangeCheck();
+
+			// hide the script field
+			GUILayout.Space(-20f);
+
+			defaultEditor.OnInspectorGUI();
+
+			if(EditorGUI.EndChangeCheck())
+			{
+				if(!string.Equals(previousNamePrefix, preferences.namePrefix, StringComparison.OrdinalIgnoreCase))
+				{
+					preferences.previousNamePrefix = previousNamePrefix;
+				}
+				if(!string.Equals(previousNameSuffix, preferences.nameSuffix, StringComparison.OrdinalIgnoreCase))
+				{
+					preferences.previousNameSuffix = previousNameSuffix;
+				}
+			}
+
+			GUILayout.Space(15f);
+
+			GUILayout.BeginHorizontal();
+			{
+				GUILayout.Space(15f);
+
+				const float buttonHeight = 25f;
+
+				if(GUILayout.Button("Apply Changes", GUILayout.Height(buttonHeight)))
+				{
+					preferences.SaveState();
+
+					if(preferences.playModeBehaviour != StrippingType.None)
+					{
+						var hierarchyFolderScript = FileUtility.FindScriptAssetForType(typeof(HierarchyFolder));
+						if(MonoImporter.GetExecutionOrder(hierarchyFolderScript) >= 0)
+						{
+							MonoImporter.SetExecutionOrder(hierarchyFolderScript, -1000);
+						}
+					}
+				}
+
+				GUILayout.Space(15f);
+
+				if(GUILayout.Button("Discard Changes", GUILayout.Height(buttonHeight)))
+				{
+					preferences.DiscardChanges();
+				}
+
+				GUILayout.Space(15f);
+
+				if(GUILayout.Button("Reset To Defaults", GUILayout.Height(buttonHeight)))
+				{
+					preferences.ResetToDefaults();
+				}
+
+				GUILayout.Space(15f);
+			}
+			GUILayout.EndHorizontal();
+
+			EditorGUIUtility.labelWidth = labelWidthWas;
+			EditorGUI.indentLevel = indentLevelWas;
+		}
+	}
+}
---
+++ Assets/_Unity Store Assets/Editor/Sisus/Hierarchy Folders/Code/PlayModeStripper.cs
@@ -0,0 +1,130 @@
+﻿#if UNITY_EDITOR
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+namespace Sisus.HierarchyFolders
+{
+	public class PlayModeStripper
+	{
+		private static PlayModeStripper instance;
+
+		private readonly HashSet<Scene> playModeStrippingHandledForScenes = new HashSet<Scene>();
+		private readonly Dictionary<Scene, HashSet<Transform>> playModeStrippingHandledForSceneRootObjects = new Dictionary<Scene, HashSet<Transform>>(1);
+		private readonly StrippingType playModeStripping = StrippingType.None;
+		private readonly PlayModeStrippingMethod playModeStrippingMethod = PlayModeStrippingMethod.EntireSceneImmediate;
+
+		public PlayModeStripper(StrippingType setStrippingType, PlayModeStrippingMethod setStrippingMethod)
+		{
+			playModeStripping = setStrippingType;
+			playModeStrippingMethod = setStrippingMethod;
+
+			if(playModeStripping == StrippingType.None)
+			{
+				return;
+			}
+
+			SceneManager.sceneLoaded += OnSceneLoaded;
+			SceneManager.sceneUnloaded += OnSceneUnloaded;
+
+			for(int s = 0, scount = SceneManager.sceneCount; s < scount; s++)
+			{
+				var scene = SceneManager.GetSceneAt(s);
+				if(scene.isLoaded && playModeStrippingHandledForScenes.Add(scene))
+				{
+					HierarchyFolderUtility.ApplyStrippingType(scene, playModeStripping);
+				}
+			}
+		}
+
+		private static PlayModeStripper Instance()
+		{
+			if(instance == null)
+			{
+				if(!EditorApplication.isPlayingOrWillChangePlaymode)
+				{
+					instance = new PlayModeStripper(StrippingType.None, default(PlayModeStrippingMethod));
+				}
+				else
+				{
+					var preferences = HierarchyFolderPreferences.Get();
+					instance = new PlayModeStripper(preferences.playModeBehaviour, preferences.playModeStrippingMethod);
+				}
+			}
+			return instance;
+		}
+
+		public static void OnSceneObjectAwake(GameObject gameObject)
+		{
+			Instance().HandleOnSceneObjectAwake(gameObject);
+		}
+
+		private void HandleOnSceneObjectAwake(GameObject gameObject)
+		{
+			if(playModeStripping == StrippingType.None)
+			{
+				return;
+			}
+
+			var scene = gameObject.scene;
+			if(playModeStrippingHandledForScenes.Contains(scene))
+			{
+				return;
+			}
+
+			switch(playModeStrippingMethod)
+			{
+				case PlayModeStrippingMethod.EntireSceneWhenLoaded:
+					if(!scene.isLoaded)
+					{
+						return;
+					}
+					break;
+				case PlayModeStrippingMethod.IndividuallyDuringAwake:
+					if(!scene.isLoaded)
+					{
+						var rootTransform = gameObject.transform.transform.root;
+						HashSet<Transform> handledRootObjects;
+						if(!playModeStrippingHandledForSceneRootObjects.TryGetValue(gameObject.scene, out handledRootObjects))
+						{
+							playModeStrippingHandledForSceneRootObjects.Add(gameObject.scene, new HashSet<Transform>(){ rootTransform });
+						}
+						else if(!handledRootObjects.Add(rootTransform))
+						{
+							return;
+						}
+						HierarchyFolderUtility.CheckForAndRemoveHierarchyFoldersInChildren(gameObject.transform.root, playModeStripping);
+						return;
+					}
+					break;
+			}
+
+			playModeStrippingHandledForScenes.Add(gameObject.scene);
+			HierarchyFolderUtility.ApplyStrippingType(gameObject.scene, playModeStripping);
+		}
+
+		private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+		{
+			#if DEV_MODE
+			Debug.Assert(playModeStripping != StrippingType.None);
+			Debug.Assert(scene.isLoaded);
+			#endif
+
+			if(!playModeStrippingHandledForScenes.Add(scene))
+			{
+				return;
+			}
+
+			playModeStrippingHandledForScenes.Add(scene);
+			HierarchyFolderUtility.ApplyStrippingType(scene, playModeStripping);
+		}
+
+		private void OnSceneUnloaded(Scene scene)
+		{
+			playModeStrippingHandledForScenes.Remove(scene);
+			playModeStrippingHandledForSceneRootObjects.Remove(scene);
+		}
+	}
+}
+#endif
